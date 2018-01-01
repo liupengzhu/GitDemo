519a0817d25db099475913d3afc179494f986725
Administrator <1129460278@qq.com>
2018-01-01 21:52:43 +0800

    测试

--- app/src/main/java/com/example/gitdemo/Demo3.java
+++
@@ -1,9 +0,0 @@
-package com.example.gitdemo;
-
-/**
- * Created by Administrator on 2018/1/1/001.
- */
-
-public class Demo3 {
-    String dem = "测试合并分支";
-}
--- app/src/main/java/com/example/gitdemo/Demo4.java
+++
@@ -1,8 +0,0 @@
-package com.example.gitdemo;
-
-/**
- * Created by Administrator on 2018/1/1/001.
- */
-
-public class Demo4 {
-}
